96897a7561f2f693a23fcec6b1661dbd7ec94d9d
CodebyKriti <kritijha406@gmail.com>
2026-01-23 20:04:04 +0530

    Create Script.js

---
+++ TO-DO -LIST/Script.js
@@ -0,0 +1,19 @@
+function addTask() {
+  let input = document.getElementById("taskInput");
+  let task = input.value;
+
+  if (task === "") {
+    alert("Please enter a task");
+    return;
+  }
+
+  let li = document.createElement("li");
+  li.innerHTML = task + " <button onclick='removeTask(this)'>X</button>";
+
+  document.getElementById("taskList").appendChild(li);
+  input.value = "";
+}
+
+function removeTask(btn) {
+  btn.parentElement.remove();
+}
